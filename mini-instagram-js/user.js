feea418ae39cf4bda76abe87136d39f8bb99c8a2
sagar joshi <sagarjo637@gmail.com>
2026-01-11 23:30:43 +0545

    added another user details

--- mini-instagram-js/user.js
+++ mini-instagram-js/user.js
@@ -25,7 +25,34 @@ const user = [
       console.log(this.isLoggedIn);
     },
   },
+  {
+    userName: "Dipesh Joshi",
+    followers: 10,
+    isLoggedIn: false,
+    logIn: function () {
+      this.isLoggedIn = true;
+    },
+    logOut: function () {
+      this.isLoggedIn = false;
+    },
+    addFollower: function () {
+      this.followers++;
+    },
+    removeFollower: function () {
+      if (this.followers > 0) {
+        this.followers--;
+      } else {
+        console.log(this.userName + "has no follower to remove!");
+      }
+    },
+    showProfile: function () {
+      console.log(this.userName);
+      console.log(this.followers);
+      console.log(this.isLoggedIn);
+    },
+  },
 ];
+
 user[0].logIn();
 user[0].addFollower();
 user[0].addFollower();
