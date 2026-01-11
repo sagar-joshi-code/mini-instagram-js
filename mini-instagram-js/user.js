676ab4f147221054691938cc1749b9281b87423b
sagar joshi <sagarjo637@gmail.com>
2026-01-11 23:34:39 +0545

    added another user details

--- mini-instagram-js/user.js
+++ mini-instagram-js/user.js
@@ -51,9 +51,35 @@ const user = [
       console.log(this.isLoggedIn);
     },
   },
+  {
+    userName: "Binod Joshi",
+    followers: 100,
+    isLoggedIn: false,
+    logIn: function () {
+      this.isLoggedIn = true;
+    },
+    logOut: function () {
+      this.isLoggedIn = false;
+    },
+    addFollower: function () {
+      this.followers++;
+    },
+    removeFollower: function () {
+      if (this.followers > 0) {
+        this.followers--;
+      } else {
+        console.log(this.userName + "has no follower to remove!");
+      }
+    },
+    showProfile: function () {
+      console.log(this.userName);
+      console.log(this.followers);
+      console.log(this.isLoggedIn);
+    },
+  },
 ];
 
-user[0].logIn();
-user[0].addFollower();
-user[0].addFollower();
-user[0].showProfile();
+// user[0].logIn();
+// user[0].addFollower();
+// user[0].addFollower();
+// user[0].showProfile();
